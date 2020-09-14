b75bf51b9edde7840e5a74a4dbad2cb95264adc9
Vayango <vayango@hotmail.fr>
2020-09-14 12:41:36 +0200

    Updates

---
+++ precache-manifest.69fe5d5b21b82e239e795892008b6a4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94806789ff240e48757fc70c8c38ac6e",
+    "url": "/WebGallery/index.html"
+  },
+  {
+    "revision": "159bcbd6bc1be2369836",
+    "url": "/WebGallery/static/css/main.d36cbcba.chunk.css"
+  },
+  {
+    "revision": "7a58055f296e09f53871",
+    "url": "/WebGallery/static/js/2.9d365671.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WebGallery/static/js/2.9d365671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "159bcbd6bc1be2369836",
+    "url": "/WebGallery/static/js/main.ec6a5ce6.chunk.js"
+  },
+  {
+    "revision": "9b3aa2f94b99b31c64ad",
+    "url": "/WebGallery/static/js/runtime-main.ec7f157d.js"
+  }
+]);
